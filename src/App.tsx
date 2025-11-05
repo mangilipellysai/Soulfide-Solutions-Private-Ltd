6c2ca35edb63c88771f46db255a4b441f3355ba7
mangilipelly saikumar <mangilipellysai.11@gmail.com>
2025-11-05 14:21:03 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { Products } from "./components/Products";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
+import { Toaster } from "./components/ui/sonner";
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Hero />
+      <About />
+      <Products />
+      <Contact />
+      <Footer />
+      <Toaster />
+    </div>
+  );
+}
